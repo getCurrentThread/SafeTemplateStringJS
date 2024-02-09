5144fdd48be463029215ce4ba81f0299ed1e4310
getCurrentThread <wak8835@gmail.com>
2024-02-09 22:26:46 +0900

    Update parseTemplateString.js

--- parseTemplateString.js
+++ parseTemplateString.js
@@ -1,41 +1,58 @@
 function parseTemplateString(templateString, data) {
   const regExp = /\${(.*?)}/g;
   return templateString.replace(regExp, (match, expression) => {
-    let [expressionString, filters = ''] = expression.split('|');
+    const [expressionString, filters = ''] = expression.split('|');
     const [propertyName, ...args] = expressionString.split(/\s*[(,]\s*/);
-    let propertyValue = data[propertyName.trim()];
+    const propertyValue = getProperty(data, propertyName.trim());
     if (propertyValue === undefined) {
       return '';
     }
     if (args[0] !== '') {
+      let result = propertyValue;
       for (let arg of args) {
         if (arg.endsWith(')')) {
-          let [funcName, ...funcArgs] = arg.split(/\s*[(,]\s*/);
-          funcName = funcName.trim();
-          funcArgs = funcArgs.map(arg => {
-            if (arg.startsWith('"') && arg.endsWith('"')) {
-              return arg.slice(1, -1);
-            } else if (arg in data) {
-              return data[arg];
-            } else {
-              return arg;
-            }
-          });
-          const func = data[funcName] || propertyValue[funcName];
-          if (func !== undefined) {
-            propertyValue = func.apply(null, [propertyValue, ...funcArgs]);
-          }
+          result = applyFunction(result, arg, data);
         } else {
-          const filterNames = filters.split(',').map(filter => filter.trim());
-          for (let filterName of filterNames) {
-            const filter = data[filterName] || propertyValue[filterName];
-            if (filter !== undefined) {
-              propertyValue = filter.apply(null, [propertyValue]);
-            }
-          }
+          result = applyFilters(result, filters, data);
         }
       }
+      return result;
     }
     return propertyValue;
   });
+}
+
+function getProperty(data, propertyName) {
+  return data[propertyName];
+}
+
+function applyFunction(value, arg, data) {
+  const [funcName, ...funcArgs] = arg.split(/\s*[(,]\s*/);
+  const trimmedFuncName = funcName.trim();
+  const processedArgs = funcArgs.map(arg => {
+    if (arg.startsWith('"') && arg.endsWith('"')) {
+      return arg.slice(1, -1);
+    } else if (arg in data) {
+      return data[arg];
+    } else {
+      return arg;
+    }
+  });
+  const func = data[trimmedFuncName] || value[trimmedFuncName];
+  if (func !== undefined) {
+    return func.apply(null, [value, ...processedArgs]);
+  }
+  return value;
+}
+
+function applyFilters(value, filters, data) {
+  const filterNames = filters.split(',').map(filter => filter.trim());
+  let result = value;
+  for (let filterName of filterNames) {
+    const filter = data[filterName] || value[filterName];
+    if (filter !== undefined) {
+      result = filter.apply(null, [result]);
+    }
+  }
+  return result;
 }
